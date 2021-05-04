4a7650cc9997caadae6ecac1d1a345c45ed0a738
Solmaz <solmaz.mousavie@gmail.com>
2021-05-04 16:54:06 +0200

    created the search component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
+import Search from "./Search"
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <div className="wrapper">
+      <div className="forecast">
         <div className="time"></div>
         <div className="date"></div>
         <div className="weatherIcon"></div>
         <div className="temperature"></div>
         <div className="description"></div>
       </div>
+      <Search/>
     </div>
   );
 }
---
+++ src/Search.js
@@ -0,0 +1,13 @@
+import "./Search.css";
+
+export default function Search() {
+  return (
+    <div className="search">
+      <div className="time"></div>
+      <div className="date"></div>
+      <div className="weatherIcon"></div>
+      <div className="temperature"></div>
+      <div className="description"></div>
+    </div>
+  );
+}
